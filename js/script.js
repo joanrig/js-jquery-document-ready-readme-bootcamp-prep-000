3ae1e2d75972935bb2b4bc661437f3f63377619d
Joan in DC <joanrigdon@gmail.com>
2019-01-17 00:44:23 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+$(document).ready(function() {
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
   <script>
    $("#text").append("this is so freaking cool.");
   </script>
+});
+
